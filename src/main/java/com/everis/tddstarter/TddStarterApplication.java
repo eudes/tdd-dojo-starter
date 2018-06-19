57594cf6e63ba6a9dd2caf7b1bf92ad87a11c16b
eudes <eudes8@gmail.com>
2018-06-19 17:48:26 +0100

    Add spring boot starter

---
+++ src/main/java/com/everis/tddstarter/TddStarterApplication.java
@@ -0,0 +1,12 @@
+package com.everis.tddstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddStarterApplication.class, args);
+	}
+}
